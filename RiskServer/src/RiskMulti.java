6badc6361804f8cb4387c941e756763a0acbf416
Starskii <jakeylakie@yahoo.com>
2020-12-07 15:37:29 -0500

    Updating Server Code

--- RiskServer/src/RiskMulti.java
+++ RiskServer/src/RiskMulti.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class RiskMulti {
+import java.net.*;
+import java.io.*;
+
+public class RiskMulti {
+
+    public static void main(String[] args) throws IOException {
+        int portNum = 1234;
+        ServerSocket serverSocket = null;
+        boolean listening = true;
+        Server server;
+
+        try {
+            serverSocket = new ServerSocket(portNum);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: " + portNum + ".");
+            System.exit(-1);
+        }
+
+        while (listening) {
+            server = new Server(serverSocket.accept());
+            Thread t = new Thread(server);
+            t.start();
+        }
+    }
 }
--- RiskServer/src/Server.java
+++ RiskServer/src/Server.java
@@ -1,2 +1,31 @@
-public class Server {
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class Server extends Thread{
+    private Socket connectionSocket;
+    int port;
+    int count = 1;
+
+    public Server(Socket connectionSocket){
+        this.connectionSocket = connectionSocket;
+    }
+
+    public void run(){
+        if (count == 1)
+            System.out.println("User connected " + connectionSocket.getInetAddress());
+        count++;
+
+        try{
+            processRequest();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    private void processRequest() throws Exception{
+
+    }
 }
