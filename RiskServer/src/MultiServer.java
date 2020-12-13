d4ebe9a975dcba4b235152a3c6cbfc69f40019ed
Starskii <jakeylakie@yahoo.com>
2020-12-13 00:15:38 -0500

    SERVER NOW WORKS!!!

---
+++ RiskServer/src/MultiServer.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class MultiServer {
+    public static ConcurrentHashMap<Integer, Integer> userList;
+    public static String game;
+
+    public static void main(String[] args) throws IOException {
+        userList = new ConcurrentHashMap<>();
+        ServerSocket serverSocket = null;
+        boolean listening = true;
+        String game = "";
+
+        try {
+            serverSocket = new ServerSocket(4999);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4999.");
+            System.exit(-1);
+        }
+
+        while (listening) {
+            Server RequestProcessor = new Server(serverSocket.accept(), userList, game);
+            Thread Worker = new Thread(RequestProcessor);
+            Worker.start();
+        }
+    }
+}
--- RiskServer/src/Server.java
+++ RiskServer/src/Server.java
@@ -1,31 +1,82 @@
 import java.io.*;
-import java.net.*;
-import java.util.*;
-import java.lang.*;
+import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 public class Server extends Thread{
     private Socket connectionSocket;
-    int port;
-    int count = 1;
+    private ObjectOutputStream outputStream;
+    private ObjectInputStream inputStream;
+    private String game;
+    ConcurrentHashMap<Integer, Integer> userList;
 
-    public Server(Socket connectionSocket){
+    public Server(Socket connectionSocket, ConcurrentHashMap<Integer, Integer> userList, String game) throws IOException {
+        this.userList = userList;
         this.connectionSocket = connectionSocket;
+        this.game = game;
+        openInputStream();
+        openOutputStream();
+    }
+
+    public void openInputStream() throws IOException {
+        inputStream = new ObjectInputStream(connectionSocket.getInputStream());
+    }
+
+    public void openOutputStream() throws IOException{
+        outputStream = new ObjectOutputStream(connectionSocket.getOutputStream());
+    }
+
+    public void closeConnection() throws IOException {
+        outputStream.close();
+        inputStream.close();
+        connectionSocket.close();
     }
 
     public void run(){
-        if (count == 1)
-            System.out.println("User connected " + connectionSocket.getInetAddress());
-        count++;
-
-        try{
+        try {
             processRequest();
-        } catch (Exception e){
-            System.out.println(e);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
-    private void processRequest() throws Exception{
+    private void processRequest() throws IOException, ClassNotFoundException {
+        String updatedGameState = (String) inputStream.readObject();
+        String ID = parseReceivedData(updatedGameState)[0];
+        int activePlayerID = Integer.parseInt(ID);
 
+        checkNewPlayer(activePlayerID);
+
+        updateServerGameState(updatedGameState);
+        updateClientGameState(updatedGameState);
+        closeConnection();
+    }
+
+    private void checkNewPlayer(int activePlayerID){
+        if(!userList.containsValue(activePlayerID))
+            userList.put(userList.size(), activePlayerID);
+    }
+
+    private void updateServerGameState(String gamestate){
+        this.game = gamestate;
+    }
+
+    private void updateClientGameState(String gamestate){
+        try {
+            outputStream.writeObject(gamestate);
+        } catch (Exception e) {
+            System.out.println("Client not listening to server.");
+        }
+    }
+
+    private String[] parseReceivedData(String message){
+        String[] gameState = message.split(":");
+        String ID = gameState[0];
+        System.out.println("Player: " + ID + " sent gamestate: ");
+        for(int i = 1; i < gameState.length; i++){
+            String[] countryInfo = gameState[i].split(" ");
+            System.out.println("Country " + countryInfo[0] + " owned by player " + countryInfo[1] + " has " + countryInfo[2] + " armies.");
+        }
+        return gameState;
     }
 }
